81c640df1a039916c0e1cdfa419724f28808855b
GuYuqi <sdguyq@163.com>
2024-11-27 14:01:07 +0800

    LuoGu
    2024/11/27

--- test.c
+++ test.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-main() {
-    int k[30] = {12, 324, 45, 6, 768, 98, 21, 34, 453, 456};
-    int count = 0, i = 1;
-    while(k[i]) {
-        if(k[i] % 2 == 0 || k[i] % 5 == 0)
-            count++;
-        i++; 
-    }
-    printf("%d %d", count, i);
-
+main(){
+	int a[10][10] = {{1}, {1, 1}}, i, j, k;
+	for(i = 2; i < 10; i++)
+		a[i][j] = a[i][0] = 1;
+	for(i = 2; i < 10; i++)
+		for(j = 0; j < 10; j++)
+			a[i][j] = a[i-1][j-1] + a[i-1][j];
+	for(i = 0; i < 10; i++){
+		for(k = 0; k <= 9-i; k++)
+			printf("  ");
+		for(j = 0; j <= i; j++)
+			printf("%4d", a[i][j]);
+		printf("\n"); 
+	} 
 }
